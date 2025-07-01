777f74e724a564cd78ee5c0cb753b0754859d4f4
IgorDyshuk <igordysuk@gmail.com>
2025-07-01 20:42:52 +0300

    add adaptive heading

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header.jsx";
 import videoBg from '/src/assets/videoBg.mp4';
-import { useEffect, useRef } from "react";
+import {useEffect, useRef} from "react";
+import cirle from "/src/assets/circle.svg";
+import arrow from "/src/assets/arrow.svg";
 
 export default function HomeSection() {
     const videoRef = useRef(null);
@@ -25,29 +27,52 @@ export default function HomeSection() {
     return (
         <section className="text-white overflow-x-hidden flex flex-col">
 
-            <div className="z-20 xl:absolute xl:inset-x-0 xl:top-0">
-                <Header />
+            <div className="z-30 xl:absolute xl:inset-x-0 xl:top-0">
+                <Header/>
             </div>
 
-            <div className="relative flex-grow overflow-hidden xl:mt-0">
+            <div className="lg:absolute z-20 container-px-clamp pt-4 lg:pt-[12rem] pb-3 w-full bg-black lg:bg-transparent">
+                <div className="grid grid-cols-[2fr_1fr]">
+                    <div className="flex flex-col">
+                        <div className="flex flex-col uppercase home-head-font-clamp leading-[1.2]">
+                            <span>custom</span>
+                            <span>software</span>
+                            <span className="text-gradient-ls">development</span>
+                        </div>
+                        <span className={"font-mulish home-font-clamp mt-7 home-info-width-clamp leading-[1.3]"}>
+                            We are a full-cycle web and mobile development company that exercises quality and communication
+                        </span>
+                    </div>
+
+                    <div className={"hidden lg:flex items-start justify-end "}>
+                        <div className={'flex items-center gap-5 font-unbounded-medium uppercase home-scroll-font-clamp'}>
+                            scroll down
+                            <button
+                                onClick={() => window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})}
+                                className="relative w-[42px] h-[42px] cursor-pointer"
+                            >
+                                <img src={cirle} alt="" className="w-full h-full object-contain"/>
+                                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                                    <img src={arrow} alt="" className="w-[22px] h-[22px]"/>
+                                </div>
+                            </button>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative flex-grow overflow-x-hidden xl:mt-0">
                 <video
                     ref={videoRef}
                     src={videoBg}
                     autoPlay loop playsInline
-                    className="relative w-full h-auto object-cover video-clamp transform"
+                    className="relative w-full h-auto object-cover video-clamp transform scale-180 md:scale-100"
                 />
                 <div className={"lg:w-full lg:bg-black lg:h-[200px]"}></div>
             </div>
 
-            <div className="absolute z-20 container-px-clamp">
-                <div className="grid grid-cols-2">
-                    <div className="flex">
-                    </div>
-                    <div className="grid grid-rows-3">
-                        {/* Ваш контент */}
-                    </div>
-                </div>
-            </div>
 
         </section>
     );
