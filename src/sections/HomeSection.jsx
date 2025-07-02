5cf91e73f43b5711f657d78e015076a83a98e62b
IgorDyshuk <igordysuk@gmail.com>
2025-07-02 11:57:17 +0300

    create adaptive video

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -11,11 +11,12 @@ export default function HomeSection() {
         function updateScale() {
             const width = window.innerWidth;
 
-            if (width >= 900) {
-                videoRef.current.style.transform = `scale(1.23)`;
+            if (width > 1024) {
+                videoRef.current.style.left = `clamp(0rem, -4.7535rem + 15.2113vw, 13.5rem)`;
+                videoRef.current.style.top = `clamp(0rem, -1.4525rem + 4.6479vw, 4.125rem)`;
             } else {
-                const scale = Math.max(1, 1.23 - (900 - width) * 0.000575);
-                videoRef.current.style.transform = `scale(${scale})`;
+                videoRef.current.style.left = `0`;
+                videoRef.current.style.top = `clamp(0rem, -1.4525rem + 4.6479vw, 4.125rem)`;
             }
         }
 
@@ -31,7 +32,8 @@ export default function HomeSection() {
                 <Header/>
             </div>
 
-            <div className="lg:absolute z-20 container-px-clamp pt-4 lg:pt-[12rem] pb-3 w-full bg-black lg:bg-transparent">
+            <div
+                className="lg:absolute z-20 container-px-clamp pt-4 lg:pt-[10rem]  2xl:pt-[12rem] pb-3 w-full bg-black lg:bg-transparent">
                 <div className="grid grid-cols-[2fr_1fr]">
                     <div className="flex flex-col">
                         <div className="flex flex-col uppercase home-head-font-clamp leading-[1.2]">
@@ -39,13 +41,15 @@ export default function HomeSection() {
                             <span>software</span>
                             <span className="text-gradient-ls">development</span>
                         </div>
-                        <span className={"font-mulish home-font-clamp mt-7 home-info-width-clamp leading-[1.3]"}>
+                        <span
+                            className={"font-mulish home-font-clamp home-info-padding-clamp home-info-width-clamp leading-[1.3]"}>
                             We are a full-cycle web and mobile development company that exercises quality and communication
                         </span>
                     </div>
 
                     <div className={"hidden lg:flex items-start justify-end "}>
-                        <div className={'flex items-center gap-5 font-unbounded-medium uppercase home-scroll-font-clamp'}>
+                        <div
+                            className={'flex items-center gap-5 font-unbounded-medium uppercase home-scroll-font-clamp'}>
                             scroll down
                             <button
                                 onClick={() => window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})}
@@ -63,14 +67,22 @@ export default function HomeSection() {
                 </div>
             </div>
 
-            <div className="relative flex-grow overflow-x-hidden xl:mt-0">
-                <video
-                    ref={videoRef}
-                    src={videoBg}
-                    autoPlay loop playsInline
-                    className="relative w-full h-auto object-cover video-clamp transform scale-180 md:scale-100"
-                />
-                <div className={"lg:w-full lg:bg-black lg:h-[200px]"}></div>
+            <div className="overflow-hidden xl:mt-0">
+                <div className={'relative ' +
+                    'pt-[4.75rem] sm:pt-[7rem] md:pt-[8rem] lg:pt-0 pb-32.5 sm:pb-50 md:pb-60 lg:pb-0'}>
+                    <div className={"absolute lg:hidden top-0 w-full bg-gradient-to-t from-transparent to-black h-20 z-10"}></div>
+                    <video
+                        ref={videoRef}
+                        src={videoBg}
+                        autoPlay loop playsInline
+                        className="relative w-full h-auto object-cover transform scale-220 lg:scale-123"
+                    />
+                    <div className={"absolute bottom-0 w-full bg-gradient-to-b from-transparent to-black " +
+                        "h-40 sm:h-70 md:h-70 lg:h-70 xl:h-80 2xl:h-100 z-10"}>
+
+                    </div>
+                    <div className={"lg:w-full lg:bg-black lg:h-[90px] xl:h-[100px] 2xl:h-[90px]"}></div>
+                </div>
             </div>
 
 
